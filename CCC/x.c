47c0e67e62abe79235e1153c02a3c8ff1907a8a3
aajiz-dm <160238870+aajiz-dm@users.noreply.github.com>
2024-05-10 15:43:19 +0500

    my c code

---
+++ CCC/x.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n=8;
+// goto(4,6);
+    printf(" \n\t Table Of %d \n \n",n);
+    printf("1 \t × \t %d \t =\t %d \n",n ,1*n);
+    printf("2 \t × \t %d \t =\t %d \n",n ,2*n);
+    printf("3 \t × \t %d \t =\t %d \n",n ,3*n);
+    printf("4 \t × \t %d \t =\t %d \n",n ,4*n);
+    printf("5 \t × \t %d \t =\t %d \n",n ,5*n);
+    printf("6 \t × \t %d \t =\t %d \n",n ,6*n);
+    printf("7 \t × \t %d \t =\t %d \n",n ,7*n);
+    printf("8 \t × \t %d \t =\t %d \n",n ,8*n);
+    printf("9 \t × \t %d \t =\t %d \n",n ,9*n);
+    printf("10 \t × \t %d \t =\t %d \n",n ,10*n);
+
+    
+    
+        return 0;
+}
